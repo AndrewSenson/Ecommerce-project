4b265fb26f2530a88f74a8cbead2197de4f6e2e2
Andrew senson <64193406+AndrewSenson@users.noreply.github.com>
2022-12-19 14:38:30 +0530

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose')
 
 module.exports={
     connectToDb:(cb)=>{
-        mongoose.connect(process.env.MONGO_URI)
+        mongoose.connect(mongodb+srv://andrewsenson:hfkn7H6Z8xcjp8vx@miniproject.odcszuk.mongodb.net/miniProject)
         .then(()=>{console.log("connected to db")
         return cb()
 
